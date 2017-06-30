6543b091cdb164cb6d560f1c8e8c9b04a7054cb0
Hodd Stefansdottir <hodd@novomaticls.com>
2017-06-30 15:57:35 +0200

    updated publish script

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo --theme=hexo-theme-tranquilpeak --buildDrafts
+hugo --theme=hugo-future-imperfect --buildDrafts
 
 # Go To Public folder
 cd public
